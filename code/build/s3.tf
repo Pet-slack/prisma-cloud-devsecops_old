424245ed72f5575764b6671caefe9b88c3669bf5
Petslack <58528619+Pet-slack@users.noreply.github.com>
2025-06-12 08:28:44 -0300

    Update s3.tf

--- code/build/s3.tf
+++ code/build/s3.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "dev_s3" {
+resource "aws_s3_bucket" "dev1_s3" {
   bucket_prefix = "dev-"
 
   tags = {
@@ -10,8 +10,8 @@ resource "aws_s3_bucket" "dev_s3" {
   }
 }
 
-resource "aws_s3_bucket_ownership_controls" "dev_s3" {
-  bucket = aws_s3_bucket.dev_s3.id
+resource "aws_s3_bucket_ownership_controls" "dev1_s3" {
+  bucket = aws_s3_bucket.dev1_s3.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
